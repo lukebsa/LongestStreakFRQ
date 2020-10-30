a14429831414ee159952983a38253a65195e9bf1
Luke Schweickart <lukeschweickart@gmail.com>
2020-10-30 11:09:50 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void longestStreak(String str) {
+        // Declare a String to print
+        // Declare an int to track longest streak =1
+        // Declare a String for which letter is repeated the most, init to first letter
+        // Declare an int for current streak =1
+        // Declare a String for current letter
+
+        // Loop for String str
+            // See if current letter equals previous letter
+            // If equals
+                // Increment current streak
+                // If current streak > longest streak
+                    // Set longest streak to current streak
+                    // Set longest letter to current letter
+            // If not equal
+                // Set current streak to 1
+                // Reset current letter
+    }
+
+}
